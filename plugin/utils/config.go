e50b4fc2b1a6d8129bc24cf9cf36fd7ff9f39994
Myzel394 <myzel394.xyllian@pm.me>
2026-06-07 14:50:44 +0200

    feat: Add more configs

--- plugin/utils/config.go
+++ plugin/utils/config.go
@@ -1,8 +1,6 @@
 package utils
 
 import (
-	"strconv"
-
 	"github.com/navidrome/navidrome/plugins/pdk/go/pdk"
 )
 
@@ -14,53 +12,15 @@ func getConfigString(key, def string) string {
 	return v
 }
 
-func ConfigSearchCountry() string {
-	return getConfigString(ConfigKeySearchCountry, DefaultSearchCountry)
-}
-
-func ConfigSearchLanguage() string {
-	return getConfigString(ConfigKeySearchLanguage, DefaultSearchLanguage)
-}
-
-func ConfigShazamCookie() string {
-	v, _ := pdk.GetConfig(ConfigKeyCookie)
+func ConfigUserToken() string {
+	v, _ := pdk.GetConfig(ConfigKeyUserToken)
 	return v
-}
-
-func ConfigSearchHTTPAcceptHeader() string {
-	return getConfigString(ConfigKeyHTTPAccept, DefaultHTTPAccept)
 }
 
 func ConfigUserAgent() string {
 	return getConfigString(ConfigKeyUserAgent, DefaultUserAgent)
 }
 
-func ConfigSearchLimit() int {
-	v, ok := pdk.GetConfig(ConfigKeySearchLimit)
-	if !ok || v == "" {
-		return DefaultSearchLimit
-	}
-
-	limit, err := strconv.Atoi(v)
-	if err != nil {
-		LogErrorf("failed to parse %s config value, defaulting to %d", ConfigKeySearchLimit, DefaultSearchLimit)
-		return DefaultSearchLimit
-	}
-
-	return limit
-}
-
-func ConfigSearchLevenshteinThreshold() float64 {
-	v, ok := pdk.GetConfig(ConfigKeyLevenshteinThreshold)
-	if !ok || v == "" {
-		return DefaultLevenshteinThreshold
-	}
-
-	threshold, err := strconv.ParseFloat(v, 64)
-	if err != nil {
-		LogErrorf("failed to parse %s config value, defaulting to %v", ConfigKeyLevenshteinThreshold, DefaultLevenshteinThreshold)
-		return DefaultLevenshteinThreshold
-	}
-
-	return threshold
+func ConfigSearchHTTPAcceptHeader() string {
+	return getConfigString(ConfigKeyHTTPAccept, DefaultHTTPAccept)
 }
--- plugin/utils/http.go
+++ plugin/utils/http.go
@@ -7,20 +7,19 @@ import (
 )
 
 func DoGetRequest(endpoint string) ([]byte, error) {
-	acceptLanguage := ConfigSearchLanguage()
 	userAgent := ConfigUserAgent()
 	httpAcceptHeader := ConfigSearchHTTPAcceptHeader()
-	shazamCookie := ConfigShazamCookie()
+	musixmatchCookie := ConfigUserToken()
 
 	req := pdk.NewHTTPRequest(pdk.MethodGet, endpoint)
 	req.SetHeader("Accept", httpAcceptHeader)
-	req.SetHeader("Accept-Language", acceptLanguage)
+	req.SetHeader("Accept-Language", "en")
 	req.SetHeader("User-Agent", userAgent)
-	req.SetHeader("Cookie", shazamCookie)
+	req.SetHeader("Cookie", "musixmatchUserToken="+musixmatchCookie)
 
 	resp := req.Send()
 	if resp.Status() != HTTPStatusOK {
-		return resp.Body(), fmt.Errorf("error code %d returned from Shazam for endpoint %s", resp.Status(), endpoint)
+		return resp.Body(), fmt.Errorf("error code %d returned from Musixmatch for endpoint %s", resp.Status(), endpoint)
 	}
 	return resp.Body(), nil
 }

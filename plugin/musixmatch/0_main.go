c2ccfc980f60f9aa558e42f5eca3a997204ee218
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2026-09-06 23:49:02 +0200

    fix: Improve analytics

--- plugin/musixmatch/0_main.go
+++ plugin/musixmatch/0_main.go
@@ -18,24 +18,31 @@ type Song struct {
 // Some lyrics just don't exist, and we do not need a failure for that.
 func FetchLyrics(input lyrics.GetLyricsRequest) (lyrics.GetLyricsResponse, error, *utils.LookupFailure, *utils.LookupSuccess, string) {
 	mobileGUIDAssignment := chooseMobileGUIDAssignment()
+	var lastErr error
+	var lastFailure *utils.LookupFailure
 	// // // Mobile
-	utils.LogInfof("FetchLyrics: lookup started for artist=%s title=%s album=%s mbz=%s", input.Track.Artist, input.Track.Title, input.Track.Album, input.Track.MBZRecordingID)
+	utils.LogInfof("FetchLyrics: lookup started source=mobile_api artist=%s title=%s album=%s mbz=%s", input.Track.Artist, input.Track.Title, input.Track.Album, input.Track.MBZRecordingID)
 	if resp, err, failure, success := fetchLyricsFromMobileAPI(input, mobileGUIDAssignment); err == nil && len(resp.Lyrics) > 0 {
 		utils.LogInfof("FetchLyrics: lookup succeeded source=mobile_api category=%s", success.CategoryValue())
 		return resp, nil, nil, success, mobileGUIDAssignment.Variant
 	} else if err != nil {
-		utils.LogErrorf("FetchLyrics: mobile API lookup failed reason=%s status=%d error=%v", failure.ReasonValue(), failure.StatusCodeValue(), err)
+		lastErr = err
+		lastFailure = failure
+		utils.LogErrorf("FetchLyrics: lookup failed source=mobile_api reason=%s status=%d error=%v", failure.ReasonValue(), failure.StatusCodeValue(), err)
 	}
 
 	// // // Website
 	if utils.ConfigUserToken() != "" {
+		utils.LogInfof("FetchLyrics: lookup started source=website artist=%s title=%s album=%s mbz=%s", input.Track.Artist, input.Track.Title, input.Track.Album, input.Track.MBZRecordingID)
 		resp, err, failure, success := fetchLyricsViaWebsiteScraping(input)
 
 		if err == nil && len(resp.Lyrics) > 0 {
-			utils.LogInfof("FetchLyrics: lookup succeeded source=mobile_api category=%s", success.CategoryValue())
+			utils.LogInfof("FetchLyrics: lookup succeeded source=website category=%s", success.CategoryValue())
 			return resp, nil, nil, success, mobileGUIDAssignment.Variant
 		} else if err != nil {
-			utils.LogErrorf("FetchLyrics: mobile API lookup failed reason=%s status=%d error=%v", failure.ReasonValue(), failure.StatusCodeValue(), err)
+			lastErr = err
+			lastFailure = failure
+			utils.LogErrorf("FetchLyrics: lookup failed source=website reason=%s status=%d error=%v", failure.ReasonValue(), failure.StatusCodeValue(), err)
 		}
 	}
 
@@ -50,13 +57,13 @@ func FetchLyrics(input lyrics.GetLyricsRequest) (lyrics.GetLyricsResponse, error
 	// if err != nil {
 	// 	utils.LogErrorf("FetchLyrics: desktop API lookup failed reason=%s status=%d error=%v", failure.ReasonValue(), failure.StatusCodeValue(), err)
 	// }
-	failure := utils.NewLookupFailure(
-		"lyrics_not_found",
-		"all_sources",
-		fmt.Errorf("No lyrics found for artist=%s title=%s album=%s mbz=%s", input.Track.Artist, input.Track.Title, input.Track.Album, input.Track.MBZRecordingID),
-	).WithPhase("all_sources")
+	if lastFailure != nil {
+		return lyrics.GetLyricsResponse{}, lastErr, lastFailure, nil, mobileGUIDAssignment.Variant
+	}
+	err := fmt.Errorf("No lyrics found for artist=%s title=%s album=%s mbz=%s", input.Track.Artist, input.Track.Title, input.Track.Album, input.Track.MBZRecordingID)
+	failure := utils.NewLookupFailure("lyrics_not_found", "all_sources", err).WithPhase("all_sources")
 	return lyrics.GetLyricsResponse{},
-		fmt.Errorf("No lyrics found for artist=%s title=%s album=%s mbz=%s", input.Track.Artist, input.Track.Title, input.Track.Album, input.Track.MBZRecordingID),
+		err,
 		failure,
 		nil,
 		mobileGUIDAssignment.Variant

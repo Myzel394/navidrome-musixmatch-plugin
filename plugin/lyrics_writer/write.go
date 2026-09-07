ea161d7bf23fdc49dd2b202aebc6ad6e91dec478
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2026-09-07 21:00:05 +0200

    fix: improve error messages

--- plugin/lyrics_writer/write.go
+++ plugin/lyrics_writer/write.go
@@ -19,7 +19,7 @@ func WriteLyrics(
 
 	library, err := host.LibraryGetLibrary(track.LibraryID)
 	if err != nil || library == nil {
-		return fmt.Errorf("could not access library %d: %w", track.LibraryID, err)
+		return fmt.Errorf("could not access your library with id %d! Make sure you gave access to the correct library. Got the error: %w", track.LibraryID, err)
 	}
 
 	trackPath := filepath.Join(library.MountPoint, track.Path)
@@ -29,13 +29,13 @@ func WriteLyrics(
 	lyricsPath := filepath.Join(filepath.Dir(trackPath), newFilename)
 	lyricsFile, err := os.Create(lyricsPath)
 	if err != nil {
-		utils.LogErrorf("failed to create lyrics file: %v", err)
+		utils.LogErrorf("Failed to write lyrics file! Make sure you have \"Allow write access\" set! Got the error: %v", err)
 		return err
 	}
 
 	_, err = lyricsFile.WriteString(lyrics.Lyrics[0].Text)
 	if err != nil {
-		utils.LogErrorf("Failed to write lyrics to file %s: %v", lyricsPath, err)
+		utils.LogErrorf("Failed to write lyrics to file %s! Make sure you have \"Allow write access\" set! Got the error: %v", lyricsPath, err)
 		return err
 	}
 

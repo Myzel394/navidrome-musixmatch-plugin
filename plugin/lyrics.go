13b6e13510b82655969e6e9394b1d8ab4eccc56c
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2026-09-07 20:27:14 +0200

    feat: Add lyrics write possibility

--- plugin/lyrics.go
+++ plugin/lyrics.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/Myzel394/navidrome-musixmatch-plugin/plugin/analytics"
+	"github.com/Myzel394/navidrome-musixmatch-plugin/plugin/lyrics_writer"
 	"github.com/Myzel394/navidrome-musixmatch-plugin/plugin/musixmatch"
 	"github.com/Myzel394/navidrome-musixmatch-plugin/plugin/utils"
 	"github.com/navidrome/navidrome/plugins/pdk/go/lyrics"
@@ -14,7 +15,7 @@ func (p *plugin) GetLyrics(input lyrics.GetLyricsRequest) (lyrics.GetLyricsRespo
 	utils.StartLogCapture()
 
 	startedAt := time.Now().UTC()
-	resp, err, failure, success, mobileGUIDVariant := musixmatch.FetchLyrics(input)
+	resp, format, err, failure, success, mobileGUIDVariant := musixmatch.FetchLyrics(input)
 	duration := time.Since(startedAt)
 
 	if err != nil {
@@ -25,9 +26,13 @@ func (p *plugin) GetLyrics(input lyrics.GetLyricsRequest) (lyrics.GetLyricsRespo
 
 	logs := utils.StopLogCapture()
 	analytics.ReportLyricsLookup(input, resp, err, failure, success, mobileGUIDVariant, startedAt, duration, logs)
+	print(fmt.Sprintf("%v", err))
 
 	if err != nil {
 		return resp, fmt.Errorf("%s: %w", utils.PluginName, err)
 	}
+
+	lyrics_writer.WriteLyrics(input.Track, resp, format)
+
 	return resp, nil
 }

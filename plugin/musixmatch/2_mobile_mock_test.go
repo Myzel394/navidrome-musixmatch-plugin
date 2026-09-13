23fecb4ebaf91cd6fa6e546175ac0fcd56223ea0
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2026-09-13 12:23:12 +0200

    fix

--- plugin/musixmatch/2_mobile_mock_test.go
+++ plugin/musixmatch/2_mobile_mock_test.go
@@ -80,9 +80,7 @@ func mockMobileAPI(t *testing.T) {
 			strings.Contains(request.URL, "apic-appmobile.musixmatch.com") &&
 			strings.Contains(request.URL, "token.get") &&
 			strings.Contains(request.URL, "app_id="+utils.DefaultMobileAppID) &&
-			strings.Contains(request.URL, "guid=xxx") &&
-			strings.Contains(request.URL, "user_language=en") &&
-			strings.Contains(request.Headers["X-Cookie"], "x-mxm-token-guid=xxx")
+			strings.Contains(request.URL, "user_language=en")
 	})).Return(&host.HTTPResponse{StatusCode: int32(utils.HTTPStatusOK), Body: tokenBody}, nil).Once()
 	host.HTTPMock.On("Send", mock.MatchedBy(func(request host.HTTPRequest) bool {
 		return request.Method == pdk.MethodGet.String() &&
